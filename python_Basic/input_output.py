e8da618098ae3b2557cb84c7e1fe33fc5083c8d8
Michaellauw1302 <michaellauw1302@gmail.com>
2025-09-03 14:41:22 +0800

    Upload Update

---
+++ python_Basic/input_output.py
@@ -0,0 +1,12 @@
+name = input("Masukan Nama Anda: ")
+print("Hallo, Selamat Datang,", name)
+
+# Printing Output (Single variable)
+s = "Bob"
+print(s)
+
+# Multiple Variables
+s = "Chris"
+usia = 23
+kota = "Mataram"
+print(s, usia, kota)
